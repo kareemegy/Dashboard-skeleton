2b9c7b5ea1b68ae29674703afec401819d3bd836
AbdelrhmanAmin <abdo.amin9991@gmail.com>
2023-03-30 14:47:48 +0200

    themeing

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,17 @@
 import Header from "./components/connectors/Header";
 import Sidebar from "./components/ui/Sidebar";
+import { useUI } from "./contexts";
 import RoutesWrapper from "./Router/RoutesWrapper";
 
 const Layout = () => {
+  const { theme } = useUI();
   return (
-    <div>
+    <div
+      data-theme={theme}
+      style={{
+        backgroundColor: "var(--background)",
+      }}
+    >
       <Header />
       <main className="flex w-full">
         <Sidebar />
--- src/contexts/UIContext.tsx
+++ src/contexts/UIContext.tsx
@@ -17,56 +17,11 @@ const defaultValue: IContext = {
 
 const UIContext = createContext(defaultValue);
 
-interface ITheme {
-  primary: string;
-  secondary: string;
-  accent: string;
-  neutral: string;
-  success: string;
-  info: string;
-  error: string;
-  warning: string;
-}
-
-const lightTheme: ITheme = {
-  primary: "#570df8",
-  secondary: "#f000b8",
-  accent: "#37cdbe",
-  neutral: "#3d4451",
-  success: "#36d399",
-  info: "#3abff8",
-  error: "#f87272",
-  warning: "#fbbd23",
-};
-
-const darkTheme: ITheme = {
-  primary: "black",
-  secondary: "#090404",
-  accent: "#343232",
-  neutral: "#666666",
-  success: "#008000",
-  info: "#0000ff",
-  error: "#ff0000",
-  warning: "#ffff00",
-};
-
 const UIProvider = ({ children }: { children: React.ReactNode }) => {
   const [theme, setTheme] = useState<"light" | "dark">("light");
-  const toggleTheme = () => { 
+  const toggleTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
     setTheme(newTheme);
-    const updateCSSvariables = (theme: ITheme) => {
-      Object.entries(theme).forEach(([key, value]) => {
-        document.documentElement.style.setProperty(`--${key}`, value);
-      });
-    };
-    if (newTheme === "dark") {
-      document.body.className = "bg-accent";
-      updateCSSvariables(darkTheme);
-    } else {
-      document.body.className = "bg-white";
-      updateCSSvariables(lightTheme);
-    }
   };
   const [isSidebarShrunk, setSidebarShrunk] = useState(false);
   const value = {
